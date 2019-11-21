695c7b48f45f9d9e964c75b318dc191f31f5d477
Javier Palomares <javier_palomares@homedepot.com>
2019-11-21 17:20:09 -0600

    Send the accept request if the prepare request is accepted

--- src/main/java/distributed/server/threads/MessageThread.java
+++ src/main/java/distributed/server/threads/MessageThread.java
@@ -1,6 +1,7 @@
 package distributed.server.threads;
 
 import distributed.server.pojos.Server;
+import distributed.server.requests.AcceptRequest;
 import distributed.server.requests.PrepareRequest;
 import distributed.server.requests.Request;
 import distributed.server.responses.AcceptResponse;
@@ -31,7 +32,6 @@ public class MessageThread implements Runnable
     @Setter(AccessLevel.PUBLIC)
     List<Server> peers;
 
-
     /**
      * Send a request to the peer. Return the response from the peer
      */
@@ -46,7 +46,7 @@ public class MessageThread implements Runnable
         try
         {
             // Get the socket
-            tcpSocket = new Socket(peer.getIpAddress(),peer.getPort());
+            tcpSocket = new Socket(peer.getIpAddress(), peer.getPort());
             PrintWriter outputWriter = new PrintWriter(tcpSocket.getOutputStream(), true);
             BufferedReader inputReader = new BufferedReader(new InputStreamReader(tcpSocket.getInputStream()));
             // Write the purchase message
@@ -56,7 +56,7 @@ public class MessageThread implements Runnable
             // Wait for the response from the server
             String response = "";
 
-            while(true)
+            while (true)
             {
                 response = inputReader.readLine();
                 if (response == null)
@@ -67,38 +67,38 @@ public class MessageThread implements Runnable
                 String resCmd = tokens[0];
                 int id = Integer.parseInt(tokens[1]);
                 String value = tokens[2];
-                if(Command.REJECT_PREPARE.getCommand().equals(resCmd))
+                if (Command.REJECT_PREPARE.getCommand().equals(resCmd))
                 {
                     // The Prepare request was rejected
-                    responseFromPeer = new PrepareResponse(id,value,false);
+                    responseFromPeer = new PrepareResponse(id, value, false);
 
-                }else if (Command.REJECT_ACCEPT.getCommand().equals(resCmd))
+                } else if (Command.REJECT_ACCEPT.getCommand().equals(resCmd))
                 {
-                    responseFromPeer = new AcceptResponse(id,value,false);
+                    responseFromPeer = new AcceptResponse(id, value, false);
                     // The accept request was rejected
-                }else if (Command.PREPARE_RESPONSE.getCommand().equals(resCmd))
+                } else if (Command.PREPARE_RESPONSE.getCommand().equals(resCmd))
                 {
-                    responseFromPeer = new PrepareResponse(id,value,true);
+                    responseFromPeer = new PrepareResponse(id, value, true);
                     // the Prepare request was accepted
-                }else if (Command.ACCEPT_RESPONSE.getCommand().equals(resCmd))
+                } else if (Command.ACCEPT_RESPONSE.getCommand().equals(resCmd))
                 {
-                    responseFromPeer = new AcceptResponse(id,value,true);
+                    responseFromPeer = new AcceptResponse(id, value, true);
                     // the accept request was accepted
                 }
             }
 
-        }catch(Exception e)
+        } catch (Exception e)
         {
             System.err.println("Unable to send msg to " + peer.toString());
             e.printStackTrace();
-        }finally
+        } finally
         {
             if (tcpSocket != null)
             {
                 try
                 {
                     tcpSocket.close();
-                }catch(Exception e)
+                } catch (Exception e)
                 {
                     System.err.println("Unable to close socket");
                     e.printStackTrace();
@@ -112,14 +112,15 @@ public class MessageThread implements Runnable
 
     /**
      * Send the request to the peers
+     *
      * @param peers
      * @return
      */
-    public List<Response> sendRequest(Request request,List<Server> peers)
+    public List<Response> sendRequest(Request request, List<Server> peers)
     {
         List<Response> responses = new ArrayList<Response>();
 
-        for(Server peer: peers)
+        for (Server peer : peers)
         {
             Response response = sendRequestToPeer(request, peer);
             responses.add(response);
@@ -128,6 +129,42 @@ public class MessageThread implements Runnable
 
     }
 
+    private boolean requestAccepted(List<Response> prepareResponses)
+    {
+        //True if the majority of peers accepted the prepare
+        int numResponses = prepareResponses.size();
+        int numAccepted = 0;
+        for (Response r : prepareResponses)
+        {
+            if (r == null || r.isResponseAccepted() == false)
+            {
+                continue;
+            }
+            numAccepted++;
+        }
+
+        if (numAccepted >= (numResponses + 1) / 2)
+        {
+            return true;
+        }
+        return false;
+    }
+
+    private void updateToGreatestPaxosId(List<Response> responses)
+    {
+        for (Response r: responses)
+        {
+            int id = r.getId();
+            if (id > ServerThread.getPaxosId())
+            {
+                ServerThread.setPaxosId(id);
+                /**
+                 * TODO: Do we need to set the value as well?
+                 */
+            }
+        }
+
+    }
 
 
    // Start the paxos algorithm to reserve the value
@@ -136,15 +173,34 @@ public class MessageThread implements Runnable
         logger.debug("Reserving value " + value);
         // Prepare request
         Request request = new PrepareRequest();
-        request.setId(Utils.getId());
+        request.setId(ServerThread.getPaxosId());
         request.setValue(value);
         // send the prepare request to all peers
         List<Response> prepareResponses = sendRequest(request, peers);
+        boolean prepareRequestAccepted = requestAccepted(prepareResponses);
+        if(prepareRequestAccepted == false)
+        {
+            // Prepare response was not accepted
+            // Update the id from the responses
+            updateToGreatestPaxosId(prepareResponses);
+            return Command.REJECT_PREPARE.getCommand();
+        }
+        // Prepare requests is good, now send accept request
         /**
          * TODO: Parse the prepare responses, then send accept response
          */
-
-        return null;
+        Request acceptRequest = new AcceptRequest();
+        acceptRequest.setId(ServerThread.getPaxosId());
+        acceptRequest.setValue(value);
+        // send the prepare request to all peers
+        List<Response> acceptResponses = sendRequest(request, peers);
+        boolean acceptRequestAccepted = requestAccepted(acceptResponses);
+        if(acceptRequestAccepted == false)
+        {
+            return Command.REJECT_ACCEPT.getCommand();
+        }
+        // The value is agreed to
+        return Command.AGREE.getCommand() + " " + value;
     }
 
 
