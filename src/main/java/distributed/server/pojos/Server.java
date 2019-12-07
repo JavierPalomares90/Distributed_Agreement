fac535bce4e46205f4e70edf04a489b823e62613
mmolteratx <32653343+mmolteratx@users.noreply.github.com>
2019-12-07 14:05:44 -0600

    Include weight as float

--- src/main/java/distributed/server/pojos/Server.java
+++ src/main/java/distributed/server/pojos/Server.java
@@ -3,6 +3,8 @@ package distributed.server.pojos;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
 
 @Data
 @AllArgsConstructor
@@ -12,7 +14,9 @@ public class Server
     private String ipAddress;
     private Integer port;
     private Integer serverId;
-    private Double weight;
+	
+    @Getter @Setter(AccessLevel.PUBLIC)
+    private Float weight;
 
     public Server(String ipAddress)
 	{
