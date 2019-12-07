a364c7f4d57ab752d1636e604eda6c976b84f98d
mmolteratx <32653343+mmolteratx@users.noreply.github.com>
2019-12-07 15:32:53 -0600

    Weighted Byz

--- src/main/java/distributed/server/byzantine/propose/ByzProposer.java
+++ src/main/java/distributed/server/byzantine/propose/ByzProposer.java
@@ -25,6 +25,22 @@ public class ByzProposer extends Proposer
      * @param acceptors
      * @return
      */
+     
+    @Override
+    private void sendAcceptRequest(AcceptRequest acceptRequest, List<Server> acceptors)
+    {
+        sendRequest(acceptRequest, acceptors);
+        // Increment the number of accepts for ourself
+        this.serverThread.getWeightedAccepts().set(this.serverThread.getWeightedAccepts().get() + this.serverThread.getOwnWeight().get());
+    }
+    
+    @Override
+    private void sendPrepareRequest(PrepareRequest prepareRequest, List<Server> acceptors)
+    {
+        sendRequest(prepareRequest, acceptors);
+        // Increment the number of promises for ourself
+        this.serverThread.getWeightedPromises().set(this.serverThread.getWeightedPromises().get() + this.serverThread.getOwnWeight().get());
+    }
 
     public boolean safe(List<Server> acceptors)
     {
