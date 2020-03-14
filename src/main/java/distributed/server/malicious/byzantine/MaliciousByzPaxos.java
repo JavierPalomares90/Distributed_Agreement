15b1507f603a497382d665b2c24b1f9ea4ea9cd1
Javier Palomares <javier.palomares.90@gmail.com>
2020-03-14 18:36:08 -0500

    Add stub of MaliciousByzPaxos

---
+++ src/main/java/distributed/server/malicious/byzantine/MaliciousByzPaxos.java
@@ -0,0 +1,23 @@
+package distributed.server.malicious.byzantine;
+
+import org.apache.log4j.Logger;
+
+import distributed.server.byzantine.ByzPaxos;
+import distributed.server.pojos.Server;
+
+import java.util.List;
+
+public class MaliciousByzPaxos extends ByzPaxos
+{
+    private static Logger logger = Logger.getLogger(MaliciousByzPaxos.class);
+
+    @Override
+    public String proposeValue(String value, List<Server> servers)
+    {
+
+        return null;
+
+    }
+
+
+}
