609f2f08d79372ba409b40f8c214912d728e1d9f
Javier Palomares <javier.palomares.90@gmail.com>
2020-03-19 20:17:44 -0500

    Added malicious distributed agreement

---
+++ src/main/java/distributed/server/malicious/MaliciousDistributedAgreement.java
@@ -0,0 +1,12 @@
+package distributed.server.malicious;
+
+import distributed.DistributedAgreement;
+
+/**
+ * Malicious implementation of the Distributed Agreement
+ */
+
+public class MaliciousDistributedAgreement extends DistributedAgreement
+{
+
+}
