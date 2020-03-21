738c9b437858d0356a82a3d8c72935fb96d44d67
Javier Palomares <javier.palomares.90@gmail.com>
2020-03-20 20:48:56 -0500

    Added more malicious threads

---
+++ src/main/java/distributed/malicious/threads/MaliciousBroadcastThread.java
@@ -0,0 +1,8 @@
+package distributed.malicious.threads;
+
+import distributed.server.threads.BroadcastThread;
+
+public class MaliciousBroadcastThread extends BroadcastThread
+{
+
+}
---
+++ src/main/java/distributed/malicious/threads/MaliciousRequestThread.java
@@ -0,0 +1,8 @@
+package distributed.malicious.threads;
+
+import distributed.server.threads.RequestThread;
+
+public class MaliciousRequestThread extends RequestThread
+{
+
+}
